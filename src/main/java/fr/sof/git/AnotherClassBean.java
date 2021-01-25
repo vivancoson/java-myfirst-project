f0de10aaf06026599bef3c67717faa8d024a0453
htc-staleb <s.taleb@htcompass.fr>
2021-01-25 23:33:14 +0100

    first commit 2nd branch

--- src/main/java/fr/sof/git/AnotherClassBean.java
+++ src/main/java/fr/sof/git/AnotherClassBean.java
@@ -2,4 +2,7 @@ package fr.sof.git;
 
 public class AnotherClassBean {
     String firstString = "cela est la premiere branche";
+    String secondString = "cela est la premiere branche";
+    String thirdString = "cela est la premiere branche";
+
 }
