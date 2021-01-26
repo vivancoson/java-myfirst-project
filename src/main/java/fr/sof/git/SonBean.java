c5bae0f8c43e122e1e6114343860e0ab05069253
htc-staleb <s.taleb@htcompass.fr>
2021-01-26 14:53:03 +0100

    single file merge

--- src/main/java/fr/sof/git/SonBean.java
+++ src/main/java/fr/sof/git/SonBean.java
@@ -3,4 +3,9 @@ package fr.sof.git;
 public class SonBean {
     String string = "ici on test";
     double d = 2;
+    String string23= "ici on test";
+    String string4 = "ici on test";
+    String string5 = "ici on test";
+    String string6 = "ici on test";
+
 }
